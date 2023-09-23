67f5d8342169111428fedbe7bb3d2f62867c0871
Jerry-Chang975 <jerry975607@gmail.com>
2023-09-23 17:57:47 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const exp = require('constants');
-const express = require('express');
+const exp = require("constants");
+const express = require("express");
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config();
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
 }
 const port = process.env.PORT || 3000;
 
 // template engine
-const { engine } = require('express-handlebars');
-app.engine('handlebars', engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.set('views', './views');
+const { engine } = require("express-handlebars");
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.render("index"));
 
 app.listen(port, () => {
   console.log(`The app is running on http://localhost:${port}`);
---
+++ readme.md
@@ -0,0 +1,3 @@
+```
+CREATE DATABASE `expense_tracker`
+```
