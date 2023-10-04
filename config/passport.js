23d1f25e486fdcde744d6bdb5c7f2f85def56c0c
Jerry-Chang975 <jerry975607@gmail.com>
2023-10-05 00:59:58 +0800

    feat: add fb oauth2 login strategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const LocalStrategy = require('passport-local');
+const FacebookStrategy = require('passport-facebook');
 const db = require('../models');
 const User = db.User;
 
@@ -22,6 +23,41 @@ passport.use(
         isMatch
           ? done(null, user)
           : done(null, false, { message: 'Incorrect email or password.' });
+      } catch (error) {
+        error.errorMessage = 'Login failed';
+        done(error);
+      }
+    }
+  )
+);
+
+// facebook strategy
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      profileFields: ['email', 'displayName'],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const email = profile.emails[0].value;
+      const name = profile.displayName;
+      try {
+        let user = await User.findOne({
+          attributes: ['id', 'name', 'email', 'password'],
+          where: { email },
+          raw: true,
+        });
+        // already registered
+        if (user) return done(null, user);
+
+        // new one
+        const randomPassword = Math.random().toString(36).slice(-8);
+        const hash = await bcrypt.hash(randomPassword, 10);
+        user = await User.create({ name, email, password: hash });
+        done(null, { id: user.id, name: user.name, email: user.email });
+
       } catch (error) {
         error.errorMessage = 'Login failed';
         done(error);
--- routes/user.js
+++ routes/user.js
@@ -21,4 +21,19 @@ router.post('/register', userControllers.register);
 
 router.post('/logout', userControllers.logout);
 
+// fb router
+router.get(
+  '/login/facebook',
+  passport.authenticate('facebook', { scope: ['email'] })
+);
+
+router.get(
+  '/oauth2/redirect/facebook',
+  passport.authenticate('facebook', {
+    successRedirect: '/records',
+    failureRedirect: '/users/login',
+    failureFlash: true,
+  })
+);
+
 module.exports = router;
