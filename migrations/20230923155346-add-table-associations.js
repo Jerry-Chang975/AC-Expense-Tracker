83f7a2259864d403dbb33e23179ba75b8ee19644
Jerry-Chang975 <jerry975607@gmail.com>
2023-10-07 23:38:38 +0800

    fix: migrate error in some env

--- migrations/20230923155346-add-table-associations.js
+++ migrations/20230923155346-add-table-associations.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Records', 'userId', {
+    await queryInterface.addColumn('records', 'userId', {
       type: Sequelize.INTEGER,
       references: {
         model: 'Users',
@@ -13,19 +13,19 @@ module.exports = {
       onUpdate: 'CASCADE',
     });
 
-    await queryInterface.addColumn('Records', 'categoryId', {
+    await queryInterface.addColumn('records', 'categoryId', {
       type: Sequelize.INTEGER,
       references: {
         model: 'Categories',
-        key: 'id',
+        key: 'id'
       },
       onDelete: 'CASCADE',
-      onUpdate: 'CASCADE',
-    });
+      onUpdate: 'CASCADE'
+    })
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Record', 'userId');
-    await queryInterface.removeColumn('Record', 'categoryId');
+    await queryInterface.removeColumn('records', 'userId')
+    await queryInterface.removeColumn('records', 'categoryId')
   },
 };
--- migrations/20230927161238-remove-record-memo-column.js
+++ migrations/20230927161238-remove-record-memo-column.js
@@ -3,12 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Records', 'memo');
+    await queryInterface.removeColumn('records', 'memo')
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Records', 'memo', {
-      type: Sequelize.STRING,
-    });
+    await queryInterface.addColumn('records', 'memo', {
+      type: Sequelize.STRING
+    })
   },
 };
